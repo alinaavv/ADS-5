e73449e29f4fb1c6c2f4b122a553f2ca21a09f3f
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-04-13 20:44:19 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,41 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+public:
+    TStack() {
+        data = new T[size];
+        topIndex = -1;
+    }
+    ~TStack() {
+        delete[] data;
+    }
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+    bool isFull() const {
+        return topIndex == size - 1;
+    }
+    void push(const T &value) {
+        if (isFull()) {
+            throw std::overflow_error("Stack is full");
+        }
+        data[++topIndex] = value;
+    }
+    T pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[topIndex--];
+    }
+    T get() const {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[topIndex];
+    }
+private:
+    T *data;
+    int topIndex;
 };
 
 #endif  // INCLUDE_TSTACK_H_
