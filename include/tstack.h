2b5ac52253a60cf128c8aba9ca3994893dd8e13f
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-06-07 15:02:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,17 +2,19 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+const int kStackSize = 100;
+
+template<typename T>
 class TStack {
  private:
-    T data[size];
+    T data[kStackSize];
     int top;
 
  public:
     TStack() : top(-1) {}
 
     bool push(const T& value) {
-        if (top >= size - 1) {
+        if (top >= kStackSize - 1) {
             return false;
         }
         data[++top] = value;
